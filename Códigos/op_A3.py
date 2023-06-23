2dd4fe61c28f4b93c9c5d78a8c7c371947310b91
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2023-06-23 17:35:34 -0300

    actualización A3

--- Códigos/op_A3.py
+++ Códigos/op_A3.py
@@ -1,7 +1,7 @@
-def op_A2(parametro):
-    dirección_origen = str(parámetros[0]) + str(parámetros[1])
+def op_A2(parámetros,memoria):
+    dirección_origen = str(parámetros[1]) + str(parámetros[2])
     dirección_origen = int(dirección_origen,16)
-    dirección_destino = str(parámetros[2]) + str(parámetros[2])
+    dirección_destino = str(parámetros[3]) + str(parámetros[4])
     dirección_destino = int(dirección_destino,16)
     if (dirección_origen <= 65534 and dirección_origen >= 1025) and (dirección_destino <= 65534 and dirección_destino >= 1025):
         memoria[dirección_destino] = memoria[dirección_origen]
