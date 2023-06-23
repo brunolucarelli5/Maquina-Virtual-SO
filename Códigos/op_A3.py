ec376d92c6f66e687bf03b0015fc05f7eda8f6e0
4GUS_m <agustinmonti2002@gmail.com>
2023-06-23 16:09:22 -0300

    Create op_A3.py

---
+++ Códigos/op_A3.py
@@ -0,0 +1,7 @@
+def op_A2(parametro):
+    dirección_origen = str(parámetros[0]) + str(parámetros[1])
+    dirección_origen = int(dirección_origen,16)
+    dirección_destino = str(parámetros[2]) + str(parámetros[2])
+    dirección_destino = int(dirección_destino,16)
+    if dirección_origen <= 65534 and dirección_destino >= 1025:
+        memoria[direcció_destino] = memoria[dirección_origen]
