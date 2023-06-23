bfcd42e9f6d2030621d9cec32cfb355d5228f5f0
4GUS_m <agustinmonti2002@gmail.com>
2023-06-23 17:37:17 -0300

    Update op_A3.py

--- Códigos/op_A3.py
+++ Códigos/op_A3.py
@@ -1,4 +1,4 @@
-def op_A2(parámetros,memoria):
+def op_A3(parámetros,memoria):
     dirección_origen = str(parámetros[1]) + str(parámetros[2])
     dirección_origen = int(dirección_origen,16)
     dirección_destino = str(parámetros[3]) + str(parámetros[4])
