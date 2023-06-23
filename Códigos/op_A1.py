3849a1eb672f78c7cefab76acb711367d07993ba
Bruno Lucarelli <brunolucarelli5@gmail.com>
2023-06-23 12:18:56 -0300

    Update operaciones A0, A1. Creación operación A2

---
+++ Códigos/op_A1.py
@@ -0,0 +1,5 @@
+def op_A1():
+    dirección = str(parámetros[0]) + str(parámetros[1])
+    dirección = int(dirección, 16)
+    if dirección <= 65534 and dirección >= 1025:
+        memoria[dirección] = ip
--- Códigos/op_A2.py
+++ Códigos/op_A2.py
@@ -1,8 +1,5 @@
-def op_A0():
+def op_A2():
     dirección = str(parámetros[0]) + str(parámetros[1])
     dirección = int(dirección, 16)
     if dirección <= 65534 and dirección >= 1025:
-        memoria[dirección] = parámetros[2]
-    
-
-
+        ip = memoria[dirección]
