6c0fe09cbae19b981655398420777cd9a3b48713
4GUS_m <agustinmonti2002@gmail.com>
2023-06-23 16:33:08 -0300

    op3

--- Códigos/op_A3.py
+++ Códigos/op_A3.py
@@ -3,5 +3,5 @@ def op_A2(parametro):
     dirección_origen = int(dirección_origen,16)
     dirección_destino = str(parámetros[2]) + str(parámetros[2])
     dirección_destino = int(dirección_destino,16)
-    if dirección_origen <= 65534 and dirección_destino >= 1025:
+    if (dirección_origen <= 65534 and dirección_origen >= 1025) and (dirección_destino <= 65534 and dirección_destino >= 1025):
         memoria[dirección_destino] = memoria[dirección_origen]
