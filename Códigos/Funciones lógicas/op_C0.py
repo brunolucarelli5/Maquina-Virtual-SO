b06c09305a9a0ac6896c0c34ee782deacee67f24
Valentino Alejandro Terreno <ninot2016@gmail.com>
2023-07-01 22:00:46 -0300

    Funciones lógicas
    Creación de funciones lógicas (op_Cn).

---
+++ Códigos/Funciones lógicas/op_C0.py
@@ -0,0 +1,15 @@
+def operacionNOT (dirección, memoria): 
+    resultado = ~(int(memoria[dirección],16)) & 255
+
+    if dirección <= 65534 and dirección >= 1025:
+       memoria[dirección] = resultado
+       xp = dirección
+    
+       return xp
+    else:
+       print(" /!\ Error en la ejecución de C0: Dirección inválida /!\ ")
+
+
+# input: 1010
+# output deseado: 0101
+
---
+++ Códigos/Funciones lógicas/op_C3.py
@@ -0,0 +1,10 @@
+def operacionXOR (dirOrigen, dirDestino, memoria):
+    resultado = int(memoria[dirOrigen], 16) ^ int(memoria[dirDestino], 16)
+
+    if (dirOrigen <= 65534 and dirOrigen >= 1025) and (dirDestino <= 65534 and dirDestino >= 1025):
+       memoria[dirDestino] = resultado
+       xp = dirDestino
+    
+       return xp
+    else:
+       print(" /!\ Error en la ejecución de C3: Dirección inválida /!\ ")
