2b773e515438f582323f6fa60808c0bdc670ebc0
Bruno Lucarelli <brunolucarelli5@gmail.com>
2023-07-04 16:49:28 -0300

    v1.53 - Corrección op_A1

--- main.py
+++ main.py
@@ -90,8 +90,15 @@ def op_A1(parámetros, memoria,ip):
     dirección = concatenar_hex(parámetros[1],parámetros[2])
     dirección = int(dirección, 16)
     if dirección <= 65534 and dirección >= 1025:
-        memoria[dirección] = hex(int(ip,16) - int("0x1",16))     #restamos 2 porque el puntero se coloca al final de los parámetros después de leerlos 
+        if len(ip) == 4:
+            ip = "0x0" + ip[len(ip) - 2 :]
+        
+        ip1 = hex(int(ip[3:4], 16))
+        ip2 = hex(int(ip[len(ip)-2:], 16))
 
+        memoria[dirección] = ip1
+        memoria[dirección + 1] = ip2
+        
         return error
     else:
         error = "1"
