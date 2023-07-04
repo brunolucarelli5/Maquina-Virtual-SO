ace9f56f600644ee0f010e124bbbbd1b56d5badd
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2023-07-04 15:25:48 -0300

    v1.501 - corrección a4

--- Códigos/main.py
+++ Códigos/main.py
@@ -146,19 +146,13 @@ def op_A4(parámetros, memoria,xp):
         contenido = memoria[dirección_origen]
         dirección_destino = int(xp, 16)
 
-        if dirección_destino >= 100 and dirección_destino <= 1024:
-            memoria[dirección_destino] = contenido
-            xp = hex(dirección_destino)
+        memoria[dirección_destino] = contenido
+        xp = hex(dirección_destino)
 
-            vector_auxiliar[0] = error
-            vector_auxiliar[1] = xp
+        vector_auxiliar[0] = error
+        vector_auxiliar[1] = xp
 
-            return vector_auxiliar
-        else:
-            error = "1"
-            print(" /!\ Error en la ejecución de A4: Dirección inválida /!\ ")
-            vector_auxiliar[0] = error
-            vector_auxiliar[1] = xp
+        return vector_auxiliar
 
     else:
         error = "1"
