3a49ca3091d6f89abf04166ed4e234dcfbfbc170
Ignacio Mosconi <134315575+ignamosconi@users.noreply.github.com>
2023-07-04 17:01:11 -0300

    v1.55 - Actualización a0

--- main.py
+++ main.py
@@ -89,12 +89,12 @@ def op_A1(parámetros, memoria,ip):
     error = "0"
     dirección = concatenar_hex(parámetros[1],parámetros[2])
     dirección = int(dirección, 16)
-    if dirección <= 65534 and dirección >= 1025:
+    if dirección <= 65533 and dirección >= 1025:
         if len(ip) == 4:
             ip = "0x0" + ip[len(ip) - 2 :]
         
-        ip1 = hex(int(ip[3:4], 16))
-        ip2 = hex(int(ip[len(ip)-2:], 16))
+        ip1 = hex(int(ip[2:3], 16))
+        ip2 = hex(int(ip[len(ip)-2:], 16) - 1)
 
         memoria[dirección] = ip1
         memoria[dirección + 1] = ip2
